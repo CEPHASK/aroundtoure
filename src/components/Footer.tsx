219b241d3fea87f43134ec5c32963de20f8f2ed9
Cephas <cephaskiprono@gmail.com>
2024-12-27 01:27:37 +0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mt-16  bg-black dark:bg-accentDark/90 flex flex-col items-center text-light dark:text-dark">
+      <span className="my-4 text-center text-white">
+        &copy; 2024 Aroundtoure. All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
