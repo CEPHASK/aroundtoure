0a5b3bff5c8549111c89e402f3201faff5b5e221
Cephas <cephaskiprono@gmail.com>
2025-01-04 22:08:12 +0300

    added cms

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const { withContentlayer } = require('next-contentlayer') 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  
 };
 
-export default nextConfig;
+module.exports = withContentlayer({ ...nextConfig})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { HeroCarousel } from "@/components/HeroCarousel";
+import { allBlogs } from "contentlayer/generated";
 
 export default function Home() {
+  console.log(allBlogs);
   return (
     <main>
       <h1 className="text-center font-extrabold">Aroundtoure</h1>
