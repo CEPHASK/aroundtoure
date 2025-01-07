82b36c59f6671fd90fdc48562e83fe224d43eb28
Cephas <cephaskiprono@gmail.com>
2025-01-07 20:37:17 +0300

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="my-10 bg-black mx-0 flex flex-col items-center text-light">
+      <h1 className="my-6 text-center text-4xl text-white">About</h1>
+    </div>
+  );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function Blog() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold underline">Blog</h1>
+    <div className="my-10 bg-black mx-0 flex flex-col items-center text-light">
+      <h1 className="my-6 text-center text-4xl text-white">Blog</h1>
     </div>
   );
 }
