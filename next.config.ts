7132e1b13272992c1bd2e5240c5bcee495918d07
Cephas <cephaskiprono@gmail.com>
2025-01-14 10:18:19 +0300

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
-const { withContentlayer } = require('next-contentlayer') 
+const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
-  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
-module.exports = withContentlayer({ ...nextConfig})
+module.exports = withContentlayer({ ...nextConfig });
