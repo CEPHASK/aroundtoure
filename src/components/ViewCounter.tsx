b84ece07e17cd26701d87a3f9e3f3ee6d448d022
Cephas <cephaskiprono@gmail.com>
2025-01-16 04:47:25 +0300

    build fix: ts undefined values

--- src/components/ViewCounter.tsx
+++ src/components/ViewCounter.tsx
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
