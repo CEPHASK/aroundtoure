6318a77cfcd1b3776fa9b86ed078c7f306e8c133
Cephas <cephaskiprono@gmail.com>
2025-01-19 09:59:42 +0300

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ export default function About() {
   return (
     <div className="my-10 bg-black mx-0 flex flex-col items-center text-light">
       <h1 className="my-6 text-center text-4xl text-white">About</h1>
+      <p>A World Tour</p>
     </div>
   );
 }
